97b243f3b473a18bb48e0d116a80073596b2a577
miracle jane <miracleokon2003@gmail.com>
2026-01-22 23:59:20 +0100

    building Walrus

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='h-20 w-full'>
+            Navbar
+        </div>  
+    </div>
+  )
+}
+
+export default Navbar
